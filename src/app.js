7c68a94e38803f60595073717a1e839d44bf8c94
Anurag Gautam <anuraggautam876@gmail.com>
2023-03-05 15:55:08 +0530

    Did Routing Configuration

--- src/app.js
+++ src/app.js
@@ -1,23 +1,54 @@
-import React from "react"
-import ReactDOM, { createRoot } from "react-dom/client"
-
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
 import Header from './components/Header';
 import Body from './components/Body';
 import {Footer} from './components/Footer';
-
-
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestaurantMenu";
 
 const AppLayout = () => {
     return (
         <>
         <Header />
-        <Body />
+        {/* Outlet */}
+        <Outlet />
         <Footer />
         </>
     );
 }
 
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        errorElement: <Error />,
+        children: [
+            {
+                path: "/",
+                element:<Body />
+            },
+            {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path: "/contact",
+                element: <Contact/>
+            },
+            {
+                path: "/restaurant/:id",
+                element: <RestaurantMenu />
+            }
+        ]
+    },
+    
+])
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />)
+root.render(<RouterProvider router={appRouter} />);
 
