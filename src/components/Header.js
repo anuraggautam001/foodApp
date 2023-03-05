f40376ddc9c83587fee7670cab91f86dbcf9de73
Anurag Gautam <anuraggautam876@gmail.com>
2023-03-05 15:58:55 +0530

    Links created for each page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Logo from '../Assets/img/logo.png';
+import { Link } from "react-router-dom";
 
 const loggedInUser = () => {
   // API call to check authentication
@@ -21,9 +22,17 @@ const Header = () => {
             <Title />
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                    
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                        
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
                     <li>Cart</li>
 
                 </ul>
